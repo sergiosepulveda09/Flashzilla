d524a747c569b71ffd78a1f84fbf893c58babed9
Sergio Sepulveda <66451506+sergiosepulveda09@users.noreply.github.com>
2021-09-07 09:42:11 -0700

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Sergio Sepulveda on 2021-09-07.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
