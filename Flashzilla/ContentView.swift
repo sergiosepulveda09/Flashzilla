c36e53e29259fc8b0598bff8b1d1a93e29854b3c
Sergio Sepulveda <66451506+sergiosepulveda09@users.noreply.github.com>
2021-09-16 02:21:48 -0700

    Counting Down with a timer

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -11,6 +11,10 @@ struct ContentView: View {
     
     @State private var cards: [Card] = [Card](repeating: Card.example, count: 10)
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var isActive = true
+
     
     var body: some View {
         ZStack {
@@ -19,6 +23,16 @@ struct ContentView: View {
                 .scaledToFill()
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(
+                        Capsule()
+                            .fill(Color.black)
+                            .opacity(0.75)
+                    )
                 ZStack {
                     ForEach(0..<cards.count, id: \.self) { index in
                         CardView(card: cards[index]) {
@@ -49,6 +63,20 @@ struct ContentView: View {
                     .padding()
                 }
             }
+        }
+        .onReceive(timer) { time in
+            guard self.isActive else  {
+                return
+            }
+            if self.timeRemaining > 0 {
+                self.timeRemaining -= 1
+            }
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+            self.isActive = false
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+            self.isActive = true
         }
     }
     
